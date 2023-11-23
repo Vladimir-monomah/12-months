c1687ae539ff7a58701458a9af6cb3ffc5580c1b
Владимир <vladimir-bykov-1999@mail.ru>
2023-11-23 14:32:06 +0300

    Обновлены данные по проекту

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_месяцев
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+    }
+}
